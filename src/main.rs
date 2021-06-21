6bd9647b911775833801d82183613405c2276715
Ingrid Rebecca Abraham <git@ingrids.email>
2021-06-21 12:56:23 +0200

    init plugins

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,33 @@ use bevy::prelude::*;
 
 fn main() {
     App::build()
-        .add_startup_system(add_squirrels.system())
-        .add_system(hello_world_system.system())
-        .add_system(greet_squirrels.system())
+        .add_plugins(DefaultPlugins) //TODO: maybe split this up?
+        .add_plugin(HelloPlugin)
         .run();
 }
 
-fn hello_world_system() {
-    println!("Hello World!");
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        //TODO:
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_squirrels.system())
+            .add_system(greet_squirrels.system());
+    }
 }
 
-fn greet_squirrels(query: Query<&Name, With<Squirrel>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_squirrels(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Squirrel>>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
