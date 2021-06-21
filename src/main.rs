8c5929268569f917c2c17ad949221d0695a4fe29
Ingrid Rebecca Abraham <git@ingrids.email>
2021-06-21 12:29:54 +0200

    add squirrel entity

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_system(hello_world_system.system()).run();
+    App::build()
+        .add_startup_system(add_squirrels.system())
+        .add_system(hello_world_system.system())
+        .add_system(greet_squirrels.system())
+        .run();
 }
 
 fn hello_world_system() {
-    println!("hello world");
+    println!("Hello World!");
+}
+
+fn greet_squirrels(query: Query<&Name, With<Squirrel>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
+}
+
+struct Squirrel;
+
+struct Name(String);
+
+fn add_squirrels(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Squirrel)
+        .insert(Name("Ingrid".to_string()));
+    commands
+        .spawn()
+        .insert(Squirrel)
+        .insert(Name("Ana".to_string()));
+    commands
+        .spawn()
+        .insert(Squirrel)
+        .insert(Name("Shep".to_string()));
 }
