bba43d80a59caac3c2aaa1ce836ee31c66f2845d
Ingrid <git@ingrids.email>
2021-07-15 16:10:03 +0200

    gen 10 sprites with random location, velocity, colour

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{core::FixedTimestep, prelude::*};
+use rand::prelude::*;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 
@@ -14,7 +15,8 @@ pub struct TamfPlugin;
 impl Plugin for TamfPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.insert_resource(Bounds { x: 0.0, y: 0.0 })
-            .add_startup_system(setup.system())
+            .add_startup_system(update_bounds.system().before("setup"))
+            .add_startup_system(setup.system().label("setup"))
             .add_system_set(
                 SystemSet::new()
                     .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
@@ -33,33 +35,37 @@ struct Squirrel {
     velocity: Vec3,
 }
 
-struct MainCamera;
-
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     bounds: ResMut<Bounds>,
-    windows: Res<Windows>,
 ) {
-    update_bounds(bounds, windows);
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    commands
-        .spawn_bundle(OrthographicCameraBundle::new_2d())
-        .insert(MainCamera);
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(Color::rgb(1.0, 0.5, 0.5).into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
-            ..Default::default()
-        })
-        .insert(Squirrel {
-            velocity: 400.0 * Vec3::new(0.5, 0.5, 0.0).normalize(),
-        });
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..10 {
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(Color::rgb(rng.gen(), rng.gen(), rng.gen()).into()),
+                transform: Transform::from_xyz(
+                    rng.gen_range(-bounds.x..bounds.x),
+                    rng.gen_range(-bounds.y..bounds.y),
+                    0.0,
+                ),
+                sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+                ..Default::default()
+            })
+            .insert(Squirrel {
+                velocity: rng.gen_range(200.0..600.0)
+                    * Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0)
+                        .normalize(),
+            });
+    }
 }
 
 fn move_squirrels(mut query: Query<(&mut Squirrel, &mut Transform)>, bounds: Res<Bounds>) {
-    if let Ok((mut squirrel, mut transform)) = query.single_mut() {
+    for (mut squirrel, mut transform) in query.iter_mut() {
         // Reverse velocity component if going out of frame
         if (transform.translation.x > bounds.x && squirrel.velocity.x > 0.0)
             || (transform.translation.x < -bounds.x && squirrel.velocity.x < 0.0)
